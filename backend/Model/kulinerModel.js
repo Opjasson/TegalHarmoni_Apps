6bee73ae12a403adc62e1781a67024c8eb98d1c5
Opjasson <azmitheusos333@gmail.com>
2025-04-13 17:33:51 +0700

    backend : add kuliner model

---
+++ backend/Model/kulinerModel.js
@@ -0,0 +1,44 @@
+import { Sequelize, UUID } from "sequelize";
+import db from "../Config/database.js";
+
+const { DataTypes } = Sequelize;
+
+const kuliner = db.define(
+    "kuliner",
+    {
+        nama: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+                len: [3, 100],
+            },
+        },
+        deskripsi: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        img: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+        maps: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                notEmpty: true,
+            },
+        },
+    },
+    {
+        freezeTableName: true,
+    }
+);
+
+export default kuliner;
