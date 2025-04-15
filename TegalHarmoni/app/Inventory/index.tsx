dba71c33c067de025555f55b23cff9cf48c45fe2
Opjasson <azmitheusos333@gmail.com>
2025-04-15 21:34:33 +0700

    frontend : add iklan in home page

--- TegalHarmoni/app/Inventory/index.tsx
+++ TegalHarmoni/app/Inventory/index.tsx
@@ -3,11 +3,13 @@ import hotelRiez from "./Img/HotelKota/riez.jpg"
 import hotelBahariIn from "./Img/HotelKota/bahariIn.jpg"
 import hotelKhas from "./Img/HotelKota/khas.jpg"
 import hotelAlexander from "./Img/HotelKota/alexander.jpg"
+import iklan from "./Img/iklan.png"
 
 export {
     splashScreen,
     hotelRiez,
     hotelBahariIn,
     hotelKhas,
-    hotelAlexander
+    hotelAlexander,
+    iklan
 }
--- TegalHarmoni/app/Pages/Home/index.tsx
+++ TegalHarmoni/app/Pages/Home/index.tsx
@@ -1,12 +1,22 @@
+import { iklan } from '@/app/Inventory';
 import React from 'react'
-import { Image, ImageBackground, Text, View } from 'react-native'
+import { Image, ImageBackground, Text, View, StyleSheet } from 'react-native'
 
 const Home = () => {
   return (
       <View style={{ flex: 1, backgroundColor: "#FBFBFB"}}>
-        
+        <Text>Penawaran Weekend</Text>
+        <View>
+          <Image src={iklan} style={styles.iklanImg}/>
+        </View>
       </View>
   );
 }
 
 export default Home
+
+const styles = StyleSheet.create({
+  iklanImg : {
+
+  }
+})
