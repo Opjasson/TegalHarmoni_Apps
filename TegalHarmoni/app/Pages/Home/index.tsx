65b72ecf5c5a75430816c32de3ddbc89e86e79d5
Opjasson <azmitheusos333@gmail.com>
2025-04-16 22:48:44 +0700

    frontend : custom page Home content

--- TegalHarmoni/app/Pages/Home/index.tsx
+++ TegalHarmoni/app/Pages/Home/index.tsx
@@ -1,22 +1,30 @@
-import { iklan } from '@/app/Inventory';
-import React from 'react'
-import { Image, ImageBackground, Text, View, StyleSheet } from 'react-native'
+import { iklan } from "@/app/Inventory";
+import React from "react";
+import { Image, ImageBackground, Text, View, StyleSheet } from "react-native";
 
 const Home = () => {
-  return (
-      <View style={{ flex: 1, backgroundColor: "#FBFBFB"}}>
-        <Text>Penawaran Weekend</Text>
-        <View>
-          <Image src={iklan} style={styles.iklanImg}/>
+    return (
+        <View style={{ flex: 1, backgroundColor: "#FBFBFB" }}>
+            <Text style={styles.head1}>Penawaran Weekend</Text>
+            <View style={styles.containerContent}>
+                <ImageBackground source={iklan} style={styles.iklanImg}>
+                </ImageBackground>
+            </View>
         </View>
-      </View>
-  );
-}
+    );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-  iklanImg : {
+  head1: {
+    fontSize: 20,
+    fontWeight: "800"
+  },
+    iklanImg: {
+        height: 200,
+    },
+    containerContent : {
 
-  }
-})
+    }
+});
