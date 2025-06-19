8a38a16fa9fb2853a86b3c903fc978b8f1447f51
Opjasson <azmitheusos333@gmail.com>
2025-06-19 17:26:30 +0700

    backend : create getAllVouchers controller

--- backend/Controllers/codeVoucher.js
+++ backend/Controllers/codeVoucher.js
@@ -1,5 +1,14 @@
 import codeVoucher from "../Model/codeVoucher.js";
 
+export const getAllVouchers = async (req, res) => {
+    try {
+        const response = await codeVoucher.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(400).json({ msg: error.message });
+    }
+};
+
 export const createVoucher = async (req, res) => {
     // const response = await codeVoucher.findAll()
     // if () {
